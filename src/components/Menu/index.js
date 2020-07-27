4fd5b5fb28569b76b11e2026ba080ebd93d2ca41
Tiago Costa <tiago.tagcosta@gmail.com>
2020-07-27 17:34:08 -0300

    initial commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import './components/ButtonLink'
+import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className='Menu'>
+            <a href='/' >
+                <img className='Logo' src={Logo} alt='Tiagoflix logo' />
+            </a>
+
+            <ButtonLink className='ButtonLink' href='/'>
+                Novo Vídeo
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu;
